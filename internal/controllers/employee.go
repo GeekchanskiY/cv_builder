eae086411a89a9855656f9d4e16f0b1c5ea7046e
Geekchanskiy <astrovskiy.04@gmail.com>
2024-06-28 16:03:25 +0300

    create employee controller created

--- internal/controllers/employee.go
+++ internal/controllers/employee.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/GeekchanskiY/cv_builder/internal/repository"
+	"github.com/GeekchanskiY/cv_builder/internal/schemas"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -25,6 +26,32 @@ func (c *EmployeeController) GetEmployees(w http.ResponseWriter, r *http.Request
 		return
 	}
 	b, err := json.Marshal(employees)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(b)
+
+}
+
+func (c *EmployeeController) CreateEmployee(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	employee := schemas.Employee{}
+
+	err := json.NewDecoder(r.Body).Decode(&employee)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.employeeRepo.CreateEmployee(employee)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	b, err := json.Marshal(employee)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
