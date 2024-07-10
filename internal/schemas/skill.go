71b927ebbfcc26c3db2c89d8c0480a96dd15aa96
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-10 17:19:39 +0300

    schema fixed

--- internal/schemas/skill.go
+++ internal/schemas/skill.go
@@ -4,13 +4,13 @@ type Skill struct {
 	Id          int    `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	ParentId    int    `json:"parent_id"`
+	ParentId    *int   `json:"parent_id"`
 }
 
 type SkillConflict struct {
 	Id       int    `json:"id"`
-	Skill1Id int    `json:"skill_1_id"`
-	Skill2Id int    `json:"skill_2_id"`
+	Skill1Id *int   `json:"skill_1_id"`
+	Skill2Id *int   `json:"skill_2_id"`
 	Comment  string `json:"comment"`
 	Priority int    `json:"priority"`
 }
