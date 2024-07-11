dabf26fcc8b9c3587263e9835533495689fca77a
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-11 15:20:24 +0300

    commented unused variable

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,9 +2,9 @@ package repository
 
 import "database/sql"
 
-const (
-	pg_err_unique_violation = "23505"
-)
+// const (
+// 	pg_err_unique_violation = "23505"
+// )
 
 type RepoHolder struct {
 	Db *sql.DB
