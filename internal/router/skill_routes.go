14999c2ed8445203a9e1d6a85465be0b0b00b822
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-11 16:58:18 +0300

    skill conflict routes added

--- internal/router/skill_routes.go
+++ internal/router/skill_routes.go
@@ -8,11 +8,20 @@ import (
 )
 
 const skillRoutePrefix = "/skills"
+const skillConflictRoutePrefix = skillRoutePrefix + "/conflicts"
 
 func CreateSkillRoutes(router *httprouter.Router, skillController *controllers.SkillController) {
+	// Conflicts
+	router.Handle(http.MethodGet, skillConflictRoutePrefix+":id", Wrapper(skillController.GetConflicts))
+	router.Handle(http.MethodPost, skillConflictRoutePrefix+"/conflicts", Wrapper(skillController.CreateConflict))
+	router.Handle(http.MethodPut, skillConflictRoutePrefix+"/conflicts", Wrapper(skillController.UpdateConflict))
+	router.Handle(http.MethodDelete, skillConflictRoutePrefix+"/conflicts", Wrapper(skillController.DeleteConflict))
+
+	// Skills
 	router.Handle(http.MethodGet, skillRoutePrefix, Wrapper(skillController.GetAll))
 	router.Handle(http.MethodPost, skillRoutePrefix, Wrapper(skillController.Create))
 	router.Handle(http.MethodPut, skillRoutePrefix, Wrapper(skillController.Update))
 	router.Handle(http.MethodDelete, skillRoutePrefix, Wrapper(skillController.Delete))
-	router.Handle(http.MethodGet, skillRoutePrefix+"/:id", Wrapper(skillController.Get))
+	router.Handle(http.MethodGet, skillRoutePrefix+"/skill/:id", Wrapper(skillController.Get))
+
 }
