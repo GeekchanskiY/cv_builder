9c1e7a3772fea5aa2138610d2e264db4be11bf63
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-11 16:58:07 +0300

    skill conflict controllers added

--- internal/controllers/skill.go
+++ internal/controllers/skill.go
@@ -36,6 +36,8 @@ func (c *SkillController) GetAll(w http.ResponseWriter, r *http.Request, p httpr
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.WriteHeader(http.StatusOK)
 	w.Write(b)
 
 }
@@ -62,13 +64,14 @@ func (c *SkillController) Create(w http.ResponseWriter, r *http.Request, p httpr
 		skill.Id = uid
 	}
 
-	w.WriteHeader(http.StatusCreated)
 	b, err := json.Marshal(skill)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
 	}
+
+	w.WriteHeader(http.StatusCreated)
 	w.Write(b)
 
 }
@@ -88,12 +91,13 @@ func (c *SkillController) Update(w http.ResponseWriter, r *http.Request, p httpr
 		return
 	}
 
-	w.WriteHeader(http.StatusCreated)
 	b, err := json.Marshal(skill)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.WriteHeader(http.StatusCreated)
 	w.Write(b)
 }
 
@@ -111,12 +115,13 @@ func (c *SkillController) Delete(w http.ResponseWriter, r *http.Request, p httpr
 		return
 	}
 
-	w.WriteHeader(http.StatusNoContent)
 	b, err := json.Marshal(skill)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.WriteHeader(http.StatusNoContent)
 	w.Write(b)
 }
 
@@ -143,5 +148,113 @@ func (c *SkillController) Get(w http.ResponseWriter, r *http.Request, p httprout
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (c *SkillController) GetConflicts(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var conflicts []schemas.SkillConflict
+	skill_id, err := strconv.Atoi(p.ByName("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid domain id"))
+		return
+	}
+
+	conflicts, err = c.skillRepo.GetConflicts(int(skill_id))
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid skill id"))
+		return
+	}
+
+	b, err := json.Marshal(conflicts)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (c *SkillController) CreateConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	conflict := schemas.SkillConflict{}
+
+	err := json.NewDecoder(r.Body).Decode(&conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	uid, err := c.skillRepo.CreateConflict(conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if uid != 0 {
+		conflict.Id = uid
+	}
+
+	b, err := json.Marshal(conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *SkillController) UpdateConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	conflict := schemas.SkillConflict{}
+	err := json.NewDecoder(r.Body).Decode(&conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.skillRepo.UpdateConflict(conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *SkillController) DeleteConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	conflict := schemas.SkillConflict{}
+	err := json.NewDecoder(r.Body).Decode(&conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.skillRepo.DeleteConflict(conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(conflict)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 	w.Write(b)
 }
