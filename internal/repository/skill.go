7f87fa215b63a06145c03e0412f9772191f4426b
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-11 16:51:49 +0300

    skill repository updated. Skill conflicts added

--- internal/repository/skill.go
+++ internal/repository/skill.go
@@ -83,3 +83,69 @@ func (repo *SkillRepository) Get(id int) (skill schemas.Skill, err error) {
 	err = row.Scan(&skill.Id, &skill.Name, &skill.Description, &skill.ParentId)
 	return skill, err
 }
+
+func (repo *SkillRepository) GetConflicts(id int) (conflicts []schemas.SkillConflict, err error) {
+	q := `SELECT id, skill_1_id, skill_2_id, comment, priority 
+	FROM skill_conflicts
+	WHERE skill_1_id = $1 OR skill_2_id = $1`
+
+	rows, err := repo.db.Query(q, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var conflict schemas.SkillConflict
+		if err := rows.Scan(&conflict.Id, &conflict.Skill1Id, &conflict.Skill2Id, &conflict.Comment, &conflict.Priority); err != nil {
+			return nil, err
+		}
+		conflicts = append(conflicts, conflict)
+	}
+
+	if err := rows.Err(); err != nil {
+		// Here's not nil, err. I'm not sure why, but:
+		// https://go.dev/doc/database/querying
+		// documentation has the same issue
+
+		return conflicts, err
+	}
+
+	return conflicts, nil
+}
+
+func (repo *SkillRepository) CreateConflict(conflict schemas.SkillConflict) (new_id int, err error) {
+	q := `INSERT INTO skill_conflicts(skill_1_id, skill_2_id, comment, priority) VALUES($1, $2, $3, $4) RETURNING id`
+
+	new_id = 0
+	err = repo.db.QueryRow(
+		q, conflict.Skill1Id, conflict.Skill2Id, conflict.Comment, conflict.Priority,
+	).Scan(&new_id)
+	if err != nil {
+		log.Println("Error creating skill in skill repository: ", err)
+		return 0, err
+	}
+
+	return int(new_id), nil
+}
+
+func (repo *SkillRepository) UpdateConflict(conflict schemas.SkillConflict) error {
+	q := `UPDATE skill_conflicts SET skill_1_id = $1, skill_2_id = $2, comment = $3, priority = $4 WHERE id = $5`
+	_, err := repo.db.Exec(
+		q,
+		conflict.Skill1Id,
+		conflict.Skill2Id,
+		conflict.Comment,
+		conflict.Priority,
+		conflict.Id,
+	)
+	return err
+}
+
+func (repo *SkillRepository) DeleteConflict(conflict schemas.SkillConflict) error {
+	q := `DELETE FROM skill_conflicts WHERE id = $1`
+	_, err := repo.db.Exec(q, conflict.Id)
+	return err
+}
