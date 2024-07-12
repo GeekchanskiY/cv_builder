371436e40288393d0519bbf6cab1e7f82ec4fcba
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-12 15:01:38 +0300

    cv skill routes added

--- internal/router/cv_routes.go
+++ internal/router/cv_routes.go
@@ -14,6 +14,10 @@ func CreateCVRoutes(router *httprouter.Router, controller *controllers.CVControl
 	router.Handle(http.MethodPost, cvRoutePrefix, Wrapper(controller.Create))
 	router.Handle(http.MethodPut, cvRoutePrefix, Wrapper(controller.Update))
 	router.Handle(http.MethodDelete, cvRoutePrefix, Wrapper(controller.Delete))
-	router.Handle(http.MethodGet, cvRoutePrefix+"/:id", Wrapper(controller.Get))
+	router.Handle(http.MethodGet, cvRoutePrefix+"/cv/:id", Wrapper(controller.Get))
+
+	router.Handle(http.MethodGet, cvRoutePrefix+"/skill/:id", Wrapper(controller.GetSkills))
+	router.Handle(http.MethodPost, cvRoutePrefix+"/skill", Wrapper(controller.AddSkill))
+	router.Handle(http.MethodDelete, cvRoutePrefix+"/skill", Wrapper(controller.DeleteSkill))
 
 }
