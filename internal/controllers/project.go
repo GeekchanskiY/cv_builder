c4f5e5414caf5035663d089cb3ed4f750ef17ffc
GeekchanskiY <astrovskiy.04@gmail.com>
2024-07-13 23:37:30 +0300

    project controller created

---
+++ internal/controllers/project.go
@@ -0,0 +1,147 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/GeekchanskiY/cv_builder/internal/repository"
+	"github.com/GeekchanskiY/cv_builder/internal/schemas"
+	"github.com/julienschmidt/httprouter"
+)
+
+type ProjectController struct {
+	projectRepo *repository.ProjectRepository
+}
+
+func CreateProjectController(repo *repository.ProjectRepository) *ProjectController {
+	return &ProjectController{
+		projectRepo: repo,
+	}
+}
+
+func (c *ProjectController) GetAll(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schemes, err := c.projectRepo.GetAll()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	b, err := json.Marshal(schemes)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (c *ProjectController) Create(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	schema := schemas.Project{}
+
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	uid, err := c.projectRepo.Create(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if uid != 0 {
+		schema.Id = uid
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *ProjectController) Update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	schema := schemas.Project{}
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.projectRepo.Update(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *ProjectController) Delete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.Project{}
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.projectRepo.Delete(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	w.Write(b)
+}
+
+func (c *ProjectController) Get(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	var schema schemas.Project
+	schema_id, err := strconv.Atoi(p.ByName("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid domain id"))
+		return
+	}
+
+	schema, err = c.projectRepo.Get(int(schema_id))
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid domain id"))
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
