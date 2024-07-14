762bb1a50561453054f8306796699c1f466bb04d
GeekchanskiY <astrovskiy.04@gmail.com>
2024-07-14 19:31:16 +0400

    employee schema updated

--- internal/schemas/employee.go
+++ internal/schemas/employee.go
@@ -4,6 +4,10 @@ type Employee struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 
+	// Used for CV decoration
+	AboutMe  string `json:"about_me"`
+	ImageUrl string `json:"image_url"`
+
 	// Used to notify if difference in experience is too large
 	RealExperience int `json:"real_experience"`
 }
