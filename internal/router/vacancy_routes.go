36dfe8a42db6358d625f3ff15cc4b3e028d2c2d0
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-15 16:28:25 +0300

    vacancy skill routers refactored

--- internal/router/vacancy_routes.go
+++ internal/router/vacancy_routes.go
@@ -8,6 +8,7 @@ import (
 )
 
 const vacancyRoutePrefix = "/vacancies"
+const vacancySkillRoutePrefix = "/vacancies/skills"
 
 func CreateVacancyRoutes(router *httprouter.Router, controller *controllers.VacancyController) {
 	router.Handle(http.MethodGet, vacancyRoutePrefix, Wrapper(controller.GetAll))
@@ -16,8 +17,9 @@ func CreateVacancyRoutes(router *httprouter.Router, controller *controllers.Vaca
 	router.Handle(http.MethodDelete, vacancyRoutePrefix, Wrapper(controller.Delete))
 	router.Handle(http.MethodGet, vacancyRoutePrefix+"/vacancy/:id", Wrapper(controller.Get))
 
-	router.Handle(http.MethodGet, vacancyRoutePrefix+"/skills/:id/", Wrapper(controller.GetSkills))
-	router.Handle(http.MethodPost, vacancyRoutePrefix+"/skills/:id/", Wrapper(controller.AddSkill))
-	router.Handle(http.MethodDelete, vacancyRoutePrefix+"/skills/:id/", Wrapper(controller.DeleteSkill))
+	router.Handle(http.MethodGet, vacancySkillRoutePrefix+"/:id", Wrapper(controller.GetSkills))
+	router.Handle(http.MethodPost, vacancySkillRoutePrefix, Wrapper(controller.AddSkill))
+	router.Handle(http.MethodDelete, vacancySkillRoutePrefix, Wrapper(controller.DeleteSkill))
+	router.Handle(http.MethodPut, vacancySkillRoutePrefix, Wrapper(controller.UpdateSkill))
 
 }
