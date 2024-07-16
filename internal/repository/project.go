84ecdfb9d626a9a960c05f3d90cd92ce6a3ef698
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-16 14:15:58 +0300

    project domains added

--- internal/repository/project.go
+++ internal/repository/project.go
@@ -71,3 +71,72 @@ func (repo *ProjectRepository) Get(id int) (schema schemas.Project, err error) {
 	err = row.Scan(&schema.Id, &schema.Name, &schema.Description)
 	return schema, err
 }
+
+func (repo *ProjectRepository) GetDomains(id int) (schemes []schemas.ProjectDomain, err error) {
+	q := `SELECT id, project_id, domain_id, comments 
+	FROM project_domains
+	WHERE project_id = $1`
+
+	rows, err := repo.db.Query(q, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var schema schemas.ProjectDomain
+		err = rows.Scan(
+			&schema.Id,
+			&schema.ProjectId,
+			&schema.DomainId,
+			&schema.Comments,
+		)
+		if err != nil {
+			return nil, err
+		}
+		schemes = append(schemes, schema)
+	}
+
+	if err := rows.Err(); err != nil {
+
+		return schemes, err
+	}
+
+	if err := rows.Close(); err != nil {
+		return schemes, err
+	}
+	return schemes, nil
+}
+
+func (repo *ProjectRepository) CreateDomains(schema schemas.ProjectDomain) (new_id int, err error) {
+	q := `INSERT INTO project_domains(project_id, domain_id, comments) VALUES($1, $2, $3) RETURNING id`
+
+	new_id = 0
+	err = repo.db.QueryRow(
+		q, schema.ProjectId, schema.DomainId, schema.Comments,
+	).Scan(&new_id)
+	if err != nil {
+		log.Println("Error creating projectDomain in repository: ", err)
+		return 0, err
+	}
+
+	return new_id, nil
+}
+
+func (repo *ProjectRepository) UpdateDomains(schema schemas.ProjectDomain) error {
+	q := `UPDATE project_domains SET project_id = $1, domain_id = $2, comments = $3 WHERE id = $4`
+	_, err := repo.db.Exec(
+		q,
+		schema.ProjectId,
+		schema.DomainId,
+		schema.Comments,
+		schema.Id,
+	)
+	return err
+}
+
+func (repo *ProjectRepository) DeleteDomains(schema schemas.ProjectDomain) error {
+	q := `DELETE FROM project_domains WHERE id = $1`
+	_, err := repo.db.Exec(q, schema.Id)
+	return err
+}
