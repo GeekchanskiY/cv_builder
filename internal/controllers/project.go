1614b8a95027a59e9374cc0a00b45b0c2300e688
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-16 14:16:12 +0300

    project domain routes added

--- internal/controllers/project.go
+++ internal/controllers/project.go
@@ -145,3 +145,109 @@ func (c *ProjectController) Get(w http.ResponseWriter, r *http.Request, p httpro
 	w.WriteHeader(http.StatusOK)
 	w.Write(b)
 }
+
+func (c *ProjectController) GetDomains(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var schemes []schemas.ProjectDomain
+	schema_id, err := strconv.Atoi(p.ByName("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid id"))
+		return
+	}
+
+	schemes, err = c.projectRepo.GetDomains(schema_id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid id"))
+		return
+	}
+
+	b, err := json.Marshal(schemes)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (c *ProjectController) CreateDomains(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.ProjectDomain{}
+
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	uid, err := c.projectRepo.CreateDomains(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if uid != 0 {
+		schema.Id = uid
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *ProjectController) UpdateDomain(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.ProjectDomain{}
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.projectRepo.UpdateDomains(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *ProjectController) DeleteDomain(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.ProjectDomain{}
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.projectRepo.DeleteDomains(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	w.Write(b)
+}
--- internal/router/project_routes.go
+++ internal/router/project_routes.go
@@ -8,12 +8,20 @@ import (
 )
 
 const projectRoutePrefix = "/projects"
+const projectDomainRoutePrefix = projectRoutePrefix + "/domains"
 
 func CreateProjectRoutes(router *httprouter.Router, controller *controllers.ProjectController) {
+	// Domains
+	router.Handle(http.MethodGet, projectDomainRoutePrefix+"/:id", Wrapper(controller.GetDomains))
+	router.Handle(http.MethodPost, projectDomainRoutePrefix, Wrapper(controller.CreateDomains))
+	router.Handle(http.MethodPut, projectDomainRoutePrefix, Wrapper(controller.UpdateDomain))
+	router.Handle(http.MethodDelete, projectDomainRoutePrefix, Wrapper(controller.DeleteDomain))
+
+	// Projects
 	router.Handle(http.MethodGet, projectRoutePrefix, Wrapper(controller.GetAll))
 	router.Handle(http.MethodPost, projectRoutePrefix, Wrapper(controller.Create))
 	router.Handle(http.MethodPut, projectRoutePrefix, Wrapper(controller.Update))
 	router.Handle(http.MethodDelete, projectRoutePrefix, Wrapper(controller.Delete))
-	router.Handle(http.MethodGet, projectRoutePrefix+"/:id", Wrapper(controller.Get))
+	router.Handle(http.MethodGet, projectRoutePrefix+"/project/:id", Wrapper(controller.Get))
 
 }
