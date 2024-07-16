8515ccf6ddd50a25a5f37f7666648f1d22502d10
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-16 17:15:02 +0300

    responsibility conflicts added

--- internal/router/responsibility_routes.go
+++ internal/router/responsibility_routes.go
@@ -8,12 +8,19 @@ import (
 )
 
 const responsibilityRoutePrefix = "/responsibilities"
+const responsibilityConflictRoutePrefix = responsibilityRoutePrefix + "/conflicts"
 
 func CreateResponsibilityRoutes(router *httprouter.Router, controller *controllers.ResponsibilityController) {
+	// Conflicts
+	router.Handle(http.MethodGet, responsibilityConflictRoutePrefix+"/:id", Wrapper(controller.GetConflicts))
+	router.Handle(http.MethodPost, responsibilityConflictRoutePrefix, Wrapper(controller.CreateConflict))
+	router.Handle(http.MethodPut, responsibilityConflictRoutePrefix, Wrapper(controller.UpdateConflict))
+	router.Handle(http.MethodDelete, responsibilityConflictRoutePrefix, Wrapper(controller.DeleteConflict))
+
 	router.Handle(http.MethodGet, responsibilityRoutePrefix, Wrapper(controller.GetAll))
 	router.Handle(http.MethodPost, responsibilityRoutePrefix, Wrapper(controller.Create))
 	router.Handle(http.MethodPut, responsibilityRoutePrefix, Wrapper(controller.Update))
 	router.Handle(http.MethodDelete, responsibilityRoutePrefix, Wrapper(controller.Delete))
-	router.Handle(http.MethodGet, responsibilityRoutePrefix+"/:id", Wrapper(controller.Get))
+	router.Handle(http.MethodGet, responsibilityRoutePrefix+"/responsibility/:id", Wrapper(controller.Get))
 
 }
