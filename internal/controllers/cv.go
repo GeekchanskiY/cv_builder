86c0d2795166bee655ac8844b0bf1e5ae1cb4a6a
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-16 17:54:53 +0300

    cv project responsibility added

--- internal/controllers/cv.go
+++ internal/controllers/cv.go
@@ -251,3 +251,109 @@ func (c *CVController) DeleteProject(w http.ResponseWriter, r *http.Request, p h
 	w.WriteHeader(http.StatusNoContent)
 	w.Write(b)
 }
+
+func (c *CVController) GetProjectResponsibilities(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var schemes []schemas.CVProjectResponsibility
+	cv_project_id, err := strconv.Atoi(p.ByName("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid cv project id"))
+		return
+	}
+
+	schemes, err = c.cvRepo.GetProjectsResponsibilities(cv_project_id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid cv project id"))
+		return
+	}
+
+	b, err := json.Marshal(schemes)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (c *CVController) CreateProjectsResponsibilities(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.CVProjectResponsibility{}
+
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	uid, err := c.cvRepo.CreateProjectResponsibility(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if uid != 0 {
+		schema.Id = uid
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *CVController) UpdateProjectResponsibility(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.CVProjectResponsibility{}
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.cvRepo.UpdateProjectResponsibility(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
+}
+
+func (c *CVController) DeleteProjectResponsibility(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	schema := schemas.CVProjectResponsibility{}
+	err := json.NewDecoder(r.Body).Decode(&schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.cvRepo.DeleteProjectResponsibility(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	w.Write(b)
+}
