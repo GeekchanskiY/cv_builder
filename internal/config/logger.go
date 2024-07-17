52d7d54e720af2cba5f41cd2a6e841d4e3279008
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-17 17:11:27 +0300

    setupLogger prettified

--- internal/config/logger.go
+++ internal/config/logger.go
@@ -13,23 +13,23 @@ import (
 
 func SetupLogger(lc fx.Lifecycle) {
 	// Logging file
-	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
 	}
 
 	// Logging to file and stdout
-	logger_output := io.MultiWriter(file, os.Stdout)
-	log.SetOutput(logger_output)
+	loggerOutput := io.MultiWriter(logFile, os.Stdout)
+	log.SetOutput(loggerOutput)
 
 	// Trace file
-	f, err := os.Create("trace.out")
+	traceFile, err := os.Create("trace.out")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Start trace
-	if err := trace.Start(f); err != nil {
+	if err = trace.Start(traceFile); err != nil {
 		fmt.Printf("failed to start trace: %v\n", err)
 		return
 	}
@@ -37,8 +37,14 @@ func SetupLogger(lc fx.Lifecycle) {
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			trace.Stop()
-			f.Close()
-			file.Close()
+			err = traceFile.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = logFile.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
 			return nil
 		},
 	})
