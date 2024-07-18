8f330db80342585683ab9978ee05315625c64a24
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-18 13:48:08 +0300

    refactored

--- internal/controllers/responsibility.go
+++ internal/controllers/responsibility.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"github.com/GeekchanskiY/cv_builder/internal/utils"
 	"net/http"
 	"strconv"
 
@@ -20,36 +21,38 @@ func CreateResponsibilityController(repo *repository.ResponsibilityRepository) *
 	}
 }
 
-func (c *ResponsibilityController) GetAll(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) GetAll(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	schemes, err := c.responsibilityRepo.GetAll()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 	b, err := json.Marshal(schemes)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
-	w.Write(b)
-
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
-func (c *ResponsibilityController) Create(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	schema := schemas.Responsibility{}
 
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	uid, err := c.responsibilityRepo.Create(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -57,124 +60,138 @@ func (c *ResponsibilityController) Create(w http.ResponseWriter, r *http.Request
 		schema.Id = uid
 	}
 
-	w.WriteHeader(http.StatusCreated)
 	b, err := json.Marshal(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
-	w.Write(b)
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 
 }
 
-func (c *ResponsibilityController) Update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) Update(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	schema := schemas.Responsibility{}
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.responsibilityRepo.Update(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	b, err := json.Marshal(schema)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	b, err := json.Marshal(schema)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) Delete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) Delete(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.Responsibility{}
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.responsibilityRepo.Delete(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func (c *ResponsibilityController) Get(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) Get(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 
 	var schema schemas.Responsibility
-	schema_id, err := strconv.Atoi(p.ByName("id"))
+	schemaId, err := strconv.Atoi(p.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
-	schema, err = c.responsibilityRepo.Get(schema_id)
+	schema, err = c.responsibilityRepo.Get(schemaId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
-	w.Write(b)
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
-func (c *ResponsibilityController) GetConflicts(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) GetConflicts(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 	var conflicts []schemas.ResponsibilityConflict
-	resp_id, err := strconv.Atoi(p.ByName("id"))
+	respId, err := strconv.Atoi(p.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
-	conflicts, err = c.responsibilityRepo.GetConflicts(resp_id)
+	conflicts, err = c.responsibilityRepo.GetConflicts(respId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(conflicts)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) CreateConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) CreateConflict(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conflict := schemas.ResponsibilityConflict{}
 
 	err := json.NewDecoder(r.Body).Decode(&conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	uid, err := c.responsibilityRepo.CreateConflict(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -184,96 +201,107 @@ func (c *ResponsibilityController) CreateConflict(w http.ResponseWriter, r *http
 
 	b, err := json.Marshal(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) UpdateConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) UpdateConflict(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conflict := schemas.ResponsibilityConflict{}
 	err := json.NewDecoder(r.Body).Decode(&conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.responsibilityRepo.UpdateConflict(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) DeleteConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) DeleteConflict(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conflict := schemas.ResponsibilityConflict{}
 	err := json.NewDecoder(r.Body).Decode(&conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.responsibilityRepo.DeleteConflict(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func (c *ResponsibilityController) GetSynonyms(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) GetSynonyms(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 	var schemes []schemas.ResponsibilitySynonym
-	resp_id, err := strconv.Atoi(p.ByName("id"))
+	respId, err := strconv.Atoi(p.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
-	schemes, err = c.responsibilityRepo.GetSynonyms(resp_id)
+	schemes, err = c.responsibilityRepo.GetSynonyms(respId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(schemes)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) CreateSynonym(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) CreateSynonym(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.ResponsibilitySynonym{}
 
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	uid, err := c.responsibilityRepo.CreateSynonym(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -283,49 +311,59 @@ func (c *ResponsibilityController) CreateSynonym(w http.ResponseWriter, r *http.
 
 	b, err := json.Marshal(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) UpdateSynonym(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) UpdateSynonym(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.ResponsibilitySynonym{}
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.responsibilityRepo.UpdateSynonym(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *ResponsibilityController) DeleteSynonym(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *ResponsibilityController) DeleteSynonym(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.ResponsibilitySynonym{}
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.responsibilityRepo.DeleteSynonym(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
