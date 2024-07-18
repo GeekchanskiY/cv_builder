59850ae439b05568f1950b42e2f16b4f6fa63e6b
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-18 13:56:31 +0300

    refactored

--- internal/controllers/skill.go
+++ internal/controllers/skill.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
+	"github.com/GeekchanskiY/cv_builder/internal/utils"
 	"net/http"
 	"strconv"
 
@@ -21,42 +21,39 @@ func CreateSkillController(repo *repository.SkillRepository) *SkillController {
 	}
 }
 
-func (c *SkillController) GetAll(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) GetAll(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	skills, err := c.skillRepo.GetAll()
 	if err != nil {
-		log.Println("error getting skills from db")
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 	b, err := json.Marshal(skills)
 	if err != nil {
-		log.Println("error marshallong skills")
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write(b)
-
 }
 
-func (c *SkillController) Create(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	skill := schemas.Skill{}
 
 	err := json.NewDecoder(r.Body).Decode(&skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	uid, err := c.skillRepo.Create(skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -66,107 +63,106 @@ func (c *SkillController) Create(w http.ResponseWriter, r *http.Request, p httpr
 
 	b, err := json.Marshal(skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
-
 }
 
-func (c *SkillController) Update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) Update(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	skill := schemas.Skill{}
 	err := json.NewDecoder(r.Body).Decode(&skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.skillRepo.Update(skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *SkillController) Delete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) Delete(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	skill := schemas.Skill{}
 	err := json.NewDecoder(r.Body).Decode(&skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.skillRepo.Delete(skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	b, err := json.Marshal(skill)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
-	w.Write(b)
 }
 
-func (c *SkillController) Get(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) Get(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 
 	var skill schemas.Skill
-	skill_id, err := strconv.Atoi(p.ByName("id"))
+	skillId, err := strconv.Atoi(p.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid domain id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
-	skill, err = c.skillRepo.Get(int(skill_id))
+	skill, err = c.skillRepo.Get(skillId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid domain id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(skill)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write(b)
 }
 
-func (c *SkillController) GetConflicts(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) GetConflicts(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 	var conflicts []schemas.SkillConflict
-	skill_id, err := strconv.Atoi(p.ByName("id"))
+	skillId, err := strconv.Atoi(p.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid skill id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
-	conflicts, err = c.skillRepo.GetConflicts(int(skill_id))
+	conflicts, err = c.skillRepo.GetConflicts(skillId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid skill id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -176,24 +172,25 @@ func (c *SkillController) GetConflicts(w http.ResponseWriter, r *http.Request, p
 		return
 	}
 
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write(b)
 }
 
-func (c *SkillController) CreateConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) CreateConflict(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conflict := schemas.SkillConflict{}
 
 	err := json.NewDecoder(r.Body).Decode(&conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	uid, err := c.skillRepo.CreateConflict(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -203,103 +200,104 @@ func (c *SkillController) CreateConflict(w http.ResponseWriter, r *http.Request,
 
 	b, err := json.Marshal(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *SkillController) UpdateConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) UpdateConflict(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conflict := schemas.SkillConflict{}
 	err := json.NewDecoder(r.Body).Decode(&conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.skillRepo.UpdateConflict(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *SkillController) DeleteConflict(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) DeleteConflict(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	conflict := schemas.SkillConflict{}
 	err := json.NewDecoder(r.Body).Decode(&conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.skillRepo.DeleteConflict(conflict)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	b, err := json.Marshal(conflict)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
-	w.Write(b)
 }
 
-func (c *SkillController) GetDomains(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) GetDomains(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 	var schemes []schemas.SkillDomain
-	skill_id, err := strconv.Atoi(p.ByName("id"))
+	skillId, err := strconv.Atoi(p.ByName("id"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid skill id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
-	schemes, err = c.skillRepo.GetDomains(skill_id)
+	schemes, err = c.skillRepo.GetDomains(skillId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Invalid skill id"))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(schemes)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write(b)
 }
 
-func (c *SkillController) CreateDomains(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) CreateDomains(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.SkillDomain{}
 
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	uid, err := c.skillRepo.CreateDomains(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
 
@@ -309,58 +307,59 @@ func (c *SkillController) CreateDomains(w http.ResponseWriter, r *http.Request, 
 
 	b, err := json.Marshal(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		utils.HandleInternalError(w, err)
 		return
 	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *SkillController) UpdateDomain(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) UpdateDomain(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.SkillDomain{}
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.skillRepo.UpdateDomains(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	b, err := json.Marshal(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	if _, err = w.Write(b); err != nil {
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write(b)
 }
 
-func (c *SkillController) DeleteDomain(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func (c *SkillController) DeleteDomain(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	schema := schemas.SkillDomain{}
 	err := json.NewDecoder(r.Body).Decode(&schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	err = c.skillRepo.DeleteDomains(schema)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	b, err := json.Marshal(schema)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.HandleInternalError(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
-	w.Write(b)
 }
