13d5473cfca19cab08c5a9f87fde6e38fad3833c
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-26 11:08:20 +0300

    cv builder controller Build init

--- internal/controllers/cv_builder.go
+++ internal/controllers/cv_builder.go
@@ -1,7 +1,15 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/GeekchanskiY/cv_builder/internal/repository"
+	"github.com/GeekchanskiY/cv_builder/internal/schemas/requests"
+	"github.com/GeekchanskiY/cv_builder/internal/usecases"
+	"github.com/GeekchanskiY/cv_builder/internal/utils"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type CVBuilderController struct {
@@ -37,6 +45,27 @@ func CreateCVBuilderController(
 	}
 }
 
-func (c *CVBuilderController) Build() {
+func (c *CVBuilderController) Build(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var err error
+	var requestData requests.BuildRequest
 
+	err = json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		utils.HandleInternalError(w, err)
+		return
+	}
+
+	cvChan := make(chan int)
+
+	// Running building a CV
+	go usecases.BuildCV(requestData.EmployeeID, requestData.VacancyID, cvChan)
+	newId := <-cvChan
+	log.Println(requestData)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte("build queued, new CV ID: " + strconv.Itoa(newId)))
+	if err != nil {
+		log.Println("Error writing response")
+	}
 }
