e16459a1c345da33dd1b364bbc59e1077663ed5c
GeekchanskiY <astrovskiy.04@gmail.com>
2024-07-27 00:57:52 +0400

    script updated

--- internal/db/scripts/fuckup.sql
+++ internal/db/scripts/fuckup.sql
@@ -10,11 +10,15 @@ delete from cv_service_responsibilities cascade;
 delete from cv_project_services cascade;
 delete from cv_projects cascade;
 
+delete from cv_build_statuses cascade;
+
+delete from vacancies cascade;
 delete from companies cascade;
 delete from cvs cascade;
 delete from domains cascade;
 delete from employees cascade;
+delete from project_services cascade;
 delete from projects cascade;
 delete from responsibilities cascade;
 delete from skills cascade;
-delete from vacancies cascade;
+
