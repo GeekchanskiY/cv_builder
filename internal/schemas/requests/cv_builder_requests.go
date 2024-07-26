ae05a50b6fe74f05aca454a64b9ca4abdc9224ed
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-26 14:22:14 +0300

    microservice factor added, validate function added

--- internal/schemas/requests/cv_builder_requests.go
+++ internal/schemas/requests/cv_builder_requests.go
@@ -1,6 +1,24 @@
 package requests
 
+import "github.com/GeekchanskiY/cv_builder/internal/schemas/interfaces"
+
 type BuildRequest struct {
+	interfaces.Validatable
 	EmployeeID int `json:"employee_id"`
 	VacancyID  int `json:"vacancy_id"`
+
+	// Value from 0 to 2.
+	// 0 - no microservice architecture at all
+	// 1 - latest 1 or 2 projects may be microservice
+	// 2 - all projects will be microservices
+	Microservices int `json:"microservices"`
+}
+
+func (r *BuildRequest) Validate() (errors []string) {
+
+	if r.Microservices < 0 || r.Microservices > 2 {
+		errors = append(errors, "microservices")
+	}
+
+	return errors
 }
