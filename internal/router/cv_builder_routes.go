088e5b4d998f69b59edcfe81f6369f0ebc8b7e64
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-29 16:57:45 +0300

    healthcheck route added

--- internal/router/cv_builder_routes.go
+++ internal/router/cv_builder_routes.go
@@ -11,5 +11,6 @@ const cvBuilderRoutePrefix = "/cv_builder"
 func CreateCvBuilderRoutes(router *httprouter.Router, controller *controllers.CVBuilderController) {
 
 	router.Handle(http.MethodPost, cvBuilderRoutePrefix+"/build", Wrapper(controller.Build))
+	router.Handle(http.MethodGet, cvBuilderRoutePrefix+"/healthcheck", Wrapper(controller.Healthcheck))
 
 }
