3bfe1dc6aeb92a19a1105316e0845a62f1003891
GeekchanskiY <astrovskiy.04@gmail.com>
2024-07-31 01:13:04 +0400

    script updated

--- internal/db/scripts/full_fuckup.sql
+++ internal/db/scripts/full_fuckup.sql
@@ -1,11 +1,12 @@
 drop table if exists responsibility_conflicts cascade ;
 drop table if exists responsibility_synonyms cascade ;
 drop table if exists project_domains cascade ;
+drop table if exists project_services cascade ;
 drop table if exists vacancy_domains cascade ;
 drop table if exists vacancy_skills cascade ;
 drop table if exists skill_conflicts cascade ;
 drop table if exists skill_domains cascade ;
-
+drop table if exists cv_build_statuses cascade;
 
 drop table if exists cv_service_responsibilities cascade ;
 drop table if exists cv_project_services cascade ;
