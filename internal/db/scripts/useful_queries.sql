ec9e2444eb8bd7183549cc85df7c3fc6d0d029a4
Geekchanskiy <astrovskiy.04@gmail.com>
2024-07-31 14:08:02 +0300

    queries added

--- internal/db/scripts/useful_queries.sql
+++ internal/db/scripts/useful_queries.sql
@@ -1,3 +1,9 @@
 
 -- used to check which projects have no domain
 select * from projects p left join project_domains pd on pd.project_id  = p.id where pd.id is null ;
+
+-- used to check which skills have no responsibilities
+select s.name, s.parent_id from skills s left join responsibilities r on r.skill_id = s.id where r.id is null;
+
+-- used to check which skills have less responsibilities
+select s.name, count(r.id) from skills s left join responsibilities r on r.skill_id = s.id group by s.name;
