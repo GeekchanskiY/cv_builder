9ea5f8f1215493c74d883877fbaffb2cac7bf369
Geekchanskiy <astrovskiy.04@gmail.com>
2024-08-02 11:31:32 +0300

    check amount of skills without responsibilities query added

--- internal/db/scripts/useful_queries.sql
+++ internal/db/scripts/useful_queries.sql
@@ -5,5 +5,8 @@ select * from projects p left join project_domains pd on pd.project_id  = p.id w
 -- used to check which skills have no responsibilities
 select s.name, s.parent_id from skills s left join responsibilities r on r.skill_id = s.id where r.id is null;
 
--- used to check which skills have less responsibilities
+-- used to check which skills have more responsibilities
 select s.name as skill_name, count(r.id) as responsibilities_amount from skills s left join responsibilities r on r.skill_id = s.id group by s.name ORDER BY responsibilities_amount DESC;
+
+-- check amount of skills without responsibilities
+select count(s.name) from skills s left join responsibilities r on r.skill_id = s.id where r is null;
